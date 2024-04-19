792ac9740ee5d808b3497498cf92b6caf0ec96fd
Akash Mondal <akashmondalnl@gmail.com>
2024-04-19 14:09:54 +0200

    Added What Letters are in correct place

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import random
 
 words = open('wordlist2.txt').read().splitlines()
-chosen_word = random.choice(words)
+generated_word = random.choice(words)
 word_check = []
+correct_letters = ['.', '.', '.', '.', '.']
 
 
-def word_to_list(chosen_word):
-    for letter in chosen_word:
+def word_to_list(generated_word):
+    for letter in generated_word:
         word_check.append(letter)
 
 
@@ -21,25 +22,26 @@ def get_user_input():
 
 def check_user_input(guess):
     empty_word = ['.','.','.','.','.']
-    guessed_letters = []
     wrong_pos = []
     i = 0
 
-    if guess == chosen_word:
+    if guess == generated_word:
         return print("That's correct.")
 
-    while i < len(chosen_word):
-        if chosen_word[i] == guess[i]:
-            # print(f"{guess[i]} is in de juiste positie!")
-            empty_word[i] = chosen_word[i]
-            guessed_letters.append(chosen_word[i])
-        elif guess[i] in chosen_word and chosen_word.count(guess[i]) >= guess.count(guess[i]):
-            # print(f"{guess[i]} is in het woord, maar niet de juiste positie!")
+    while i < len(generated_word):
+        if generated_word[i] == guess[i]:
+            empty_word[i] = guess[i]
+            correct_letters[i] = guess[i]
+
+        elif guess[i] in generated_word and generated_word.count(guess[i]) >= guess.count(guess[i]):
             wrong_pos.append(guess[i])
         else:
             print(f"{guess[i]} is niet in het woord")
         i += 1
+
     print(empty_word, wrong_pos)
+    print(correct_letters)
     get_user_input()
 
+print(generated_word)
 get_user_input()
